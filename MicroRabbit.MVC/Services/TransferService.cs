76b4ddc0308cdb447f9847959d993ca22da249e6
Larissa Chiaradia Da Silva <larissa.silva@fcamara.com>
2024-05-22 11:49:38 -0300

    Feat: Create and Implemented Project MVC.

---
+++ MicroRabbit.MVC/Services/TransferService.cs
@@ -0,0 +1,25 @@
+﻿using MicroRabbit.MVC.Models.DTOs;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MicroRabbit.MVC.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly HttpClient _apiClient;
+
+        public TransferService(HttpClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public async Task Transfer(TransferDTO transferDTO)
+        {
+            var uri = "https://localhost:7265/api/Banking";
+            var transferContent = new StringContent(JsonConvert.SerializeObject(transferDTO),
+                                                    Encoding.UTF8, "application/json");
+            var response = await _apiClient.PostAsync(uri, transferContent);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
